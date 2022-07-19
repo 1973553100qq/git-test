07b30c48627d34903ab0919c7f01664b4e32bb51
1973553100qq <109519895+1973553100qq@users.noreply.github.com>
2022-07-19 17:38:26 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,10 +5,8 @@ public class GitTest {
         System.out.println("hello git1");
         System.out.println("hello git2");
         System.out.println("hello git3");
-        System.out.println("hello git4");
-        System.out.println("master test");
-        System.out.println("pull test");
-            System.out.println("push test");
+
+
 
     }
 }
